938bcee3981133a35c43217c01933c44d94f76f9
Julia Gokhberg <juliagokhberg@Julias-MacBook-Air-2.local>
2014-06-10 10:56:48 -0400

    User fix

--- User.java
+++ User.java
@@ -3,10 +3,10 @@ import java.util.*;
 
 public class User{
     private static String username = "";
-    
+    private static Scanner sc = new Scanner(System.in);
     
     public static void welcome(){
-	Scanner sc = new Scanner(System.in);
+	
 	if (username.equals("")){
 	    System.out.println("Welcome to your Student Organizer! Enter a new username!");
 	    username = sc.nextLine();
