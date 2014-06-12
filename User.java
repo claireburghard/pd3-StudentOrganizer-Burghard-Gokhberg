f577f1937c7b530ee7540f0afb82b3700488ef03
Julia Gokhberg <juliagokhberg@Julias-MacBook-Air-2.local>
2014-06-12 19:14:08 -0400

    Worked on the user class

--- User.java
+++ User.java
@@ -16,6 +16,23 @@ public class User{
 	}
     }
 
+    public static void addAnAsmt(){
+	System.out.println("What is the name of your assignment?");
+	String name = sc.nextLine();
+	System.out.println("What is the due date of your assignment?");
+	System.out.println("Please enter the date in this order:");
+	System.out.println("DAY, MONTH, YEAR, HOUR, MINUTE");
+	int d = sc.nextInt();
+	int m = sc.nextInt();
+	int y = sc.nextInt();
+	int h = sc.nextInt();
+	int min = sc.nextInt();
+	System.out.println("Adding your assignment now...");
+	DueDate date = new DueDate(y, m, d, h, min);
+	Assignment a = new Assignment(name, date);
+	//Claire can you add the part of the code that adds this assignment to the ToDoList?
+	//THANKS :D
+    }
 
     public static void main(String[] args){
 	welcome();
@@ -28,6 +45,10 @@ public class User{
 	System.out.println("Save the contents of My Backpack (save)");
 	System.out.println("Exit My Backpack (exit)");
 	String response = sc.nextLine();
+	if (response.equals("add")){
+		addAnAsmt();
+	}
+	    
 	
 	
     }
